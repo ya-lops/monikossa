e6afdc33301901ad253c6c552195fe6e73127491
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-02-08 22:16:09 +0300

    ELEARN init [draft]

---
+++ src/data/elearn/teachers.js
@@ -0,0 +1,32 @@
+const teachers = [
+  {
+    image: 'https://www.globalbankingandfinance.com/wp-content/uploads/2018/07/john-smith.jpg',
+    name: 'Джон Смит',
+    skill: '10 лет',
+    education: 'Университет Мичигана, степень бакалавра в области лингвистики',
+    accent: 'американский'
+  },
+  {
+    image: 'https://avatars.mds.yandex.net/i?id=f2915d6ba41f34083a0600529448c72b49b90310-9721849-images-thumbs&n=13',
+    name: 'Элизабет Джонс',
+    skill: '5 лет',
+    education: 'Университет Лондона, степень магистра в области преподавания английского как второго языка',
+    accent: 'британский'
+  },
+  {
+    image: 'https://images.prismic.io/nurole/a687952f-2974-40c7-a59e-ab29046124cc_original_lord_browne_photo_1600x900.jpeg?auto=compress,format',
+    name: 'Джеймс Браун',
+    skill: '17 лет',
+    education: 'Университет Сиднея, степень бакалавра в области лингвистики',
+    accent: 'австралийский'
+  },
+  {
+    image: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AALHpfZ.img',
+    name: 'Сара Уилсон',
+    skill: '3 года',
+    education: 'Университет Торонто, степень бакалавра в области лингвистики',
+    accent: 'канадский'
+  },
+]
+
+export default teachers;
