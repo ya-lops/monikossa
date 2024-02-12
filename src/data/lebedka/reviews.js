df19ef40007d3390c0b2287e3c2e13655df12e67
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-02-12 20:19:33 +0300

    LEBEDKA init draft

---
+++ src/data/lebedka/reviews.js
@@ -0,0 +1,22 @@
+const reviews = [
+  {
+    text: 'Я очень доволен своим опытом обучения в онлайн-школе английского языка The System. Учителя очень профессиональны и всегда готовы помочь. Я заметил значительное улучшение в своем английском языке после нескольких месяцев обучения. Я рекомендую эту школу всем, кто хочет изучать английский язык.',
+    name: "Иван C.",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/75/759f7cc384896611499df40e7c5c3e5ed4e03e2e_full.jpg",
+  },
+  {
+    text: 'Учителя очень терпеливы и всегда находят индивидуальный подход к каждому студенту. Я чувствую, что мой английский язык стал намного лучше',
+    name: "Петр П.",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/7e/7e1136c2bad539b7287c65a9fc1179bf91c188b8_full.jpg",
+  },
+  {
+    text: 'Я очень доволен своим опытом обучения в онлайн-школе английского языка The System. Учителя очень дружелюбны и всегда готовы помочь. Я заметил значительное улучшение в своем английском языке после нескольких месяцев обучения. Я рекомендую эту школу всем, кто хочет изучать английский язык.',
+    name: "Анна Б.",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/ac/acc84a608318e40f1e1cce761f2f99e1eae40be8_full.jpg",
+  },
+];
+
+export default reviews;
